41d3149c18d199f8a67b261b3256286578bd5424
zalyz <54811025+zalyz@users.noreply.github.com>
2020-04-21 22:55:39 +0300

    lab3

---
+++ Lab3/Lab3/FileReading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Lab3
+{
+    public static class FileReading
+    {
+        public static string ReadFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static void SaveInFile(string path, string text)
+        {
+            using (StreamWriter writer = new StreamWriter(Path.GetFullPath(path)))
+            {
+                writer.Write(text);
+            }
+        }
+    }
+}
