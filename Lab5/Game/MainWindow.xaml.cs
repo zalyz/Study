56a53b1c87b06eaa297a625249e6a7dd66aa682d
zalyz <54811025+zalyz@users.noreply.github.com>
2020-06-01 10:47:31 +0300

    Add files via upload

---
+++ Lab5/Game/MainWindow.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+using System.Windows.Threading;
+
+namespace Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly List<Ellipse> arrayOfWhite = new List<Ellipse>();
+
+        private static readonly List<Ellipse> arrayOfBlack = new List<Ellipse>();
+
+        static Ellipse ellipse = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                arrayOfWhite.Add(new Ellipse()
+                {
+                    Fill = new SolidColorBrush(Colors.Pink),
+                    Height = 60,
+                    Width = 60,
+                });
+
+                arrayOfWhite[i].PreviewMouseRightButtonUp += Ellips_MuseRightButtonUp;
+
+                arrayOfBlack.Add(new Ellipse()
+                {
+                    Fill = new SolidColorBrush(Colors.Black),
+                    Height = 60,
+                    Width = 60,
+                });
+
+                arrayOfBlack[i].PreviewMouseRightButtonUp += Ellips_MuseRightButtonUp;
+            }
+
+            SetPositionsOfWhite();
+            SetPositionsOfBlack();
+            for (int i = 0; i < arrayOfWhite.Count; i++)
+            {
+                gameField.Children.Add(arrayOfWhite[i]);
+                gameField.Children.Add(arrayOfBlack[i]);
+            }
+        }
+
+        private void Ellips_MuseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            arrayOfWhite.Remove((Ellipse)sender);
+            arrayOfBlack.Remove((Ellipse)sender);
+            gameField.Children.Remove((Ellipse)sender);
+
+            if (IsEndOfGame())
+            {
+                foreach (var blackEllips in arrayOfBlack)
+                {
+                    gameField.Children.Remove(blackEllips);
+                }
+
+                foreach (var whiteEllips in arrayOfWhite)
+                {
+                    gameField.Children.Remove(whiteEllips);
+                }
+
+                arrayOfWhite.Clear();
+                arrayOfBlack.Clear();
+
+                if (MessageBox.Show("Конец игры, начать новую?", "Конец игры", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    Button_Click(null, null);
+                }
+            }
+        }
+
+        private bool IsEndOfGame()
+        {
+            if (arrayOfBlack.Count == 0)
+                return true;
+            if (arrayOfWhite.Count == 0)
+                return true;
+            return false;
+        }
+
+        private void SetPositionsOfWhite()
+        {
+            arrayOfWhite[0].SetValue(Grid.RowProperty, 0);
+            arrayOfWhite[1].SetValue(Grid.RowProperty, 0);
+            arrayOfWhite[2].SetValue(Grid.RowProperty, 0);
+            arrayOfWhite[3].SetValue(Grid.RowProperty, 0);
+
+            arrayOfWhite[0].SetValue(Grid.ColumnProperty, 1);
+            arrayOfWhite[1].SetValue(Grid.ColumnProperty, 3);
+            arrayOfWhite[2].SetValue(Grid.ColumnProperty, 5);
+            arrayOfWhite[3].SetValue(Grid.ColumnProperty, 7);
+
+            arrayOfWhite[4].SetValue(Grid.RowProperty, 1);
+            arrayOfWhite[5].SetValue(Grid.RowProperty, 1);
+            arrayOfWhite[6].SetValue(Grid.RowProperty, 1);
+            arrayOfWhite[7].SetValue(Grid.RowProperty, 1);
+
+            arrayOfWhite[4].SetValue(Grid.ColumnProperty, 0);
+            arrayOfWhite[5].SetValue(Grid.ColumnProperty, 2);
+            arrayOfWhite[6].SetValue(Grid.ColumnProperty, 4);
+            arrayOfWhite[7].SetValue(Grid.ColumnProperty, 6);
+
+            arrayOfWhite[8].SetValue(Grid.RowProperty, 2);
+            arrayOfWhite[9].SetValue(Grid.RowProperty, 2);
+            arrayOfWhite[10].SetValue(Grid.RowProperty, 2);
+            arrayOfWhite[11].SetValue(Grid.RowProperty, 2);
+
+            arrayOfWhite[8].SetValue(Grid.ColumnProperty, 1);
+            arrayOfWhite[9].SetValue(Grid.ColumnProperty, 3);
+            arrayOfWhite[10].SetValue(Grid.ColumnProperty, 5);
+            arrayOfWhite[11].SetValue(Grid.ColumnProperty, 7);
+        }
+
+        private void SetPositionsOfBlack()
+        {
+            arrayOfBlack[0].SetValue(Grid.RowProperty, 7);
+            arrayOfBlack[1].SetValue(Grid.RowProperty, 7);
+            arrayOfBlack[2].SetValue(Grid.RowProperty, 7);
+            arrayOfBlack[3].SetValue(Grid.RowProperty, 7);
+
+            arrayOfBlack[0].SetValue(Grid.ColumnProperty, 0);
+            arrayOfBlack[1].SetValue(Grid.ColumnProperty, 2);
+            arrayOfBlack[2].SetValue(Grid.ColumnProperty, 4);
+            arrayOfBlack[3].SetValue(Grid.ColumnProperty, 6);
+
+            arrayOfBlack[4].SetValue(Grid.RowProperty, 6);
+            arrayOfBlack[5].SetValue(Grid.RowProperty, 6);
+            arrayOfBlack[6].SetValue(Grid.RowProperty, 6);
+            arrayOfBlack[7].SetValue(Grid.RowProperty, 6);
+
+            arrayOfBlack[4].SetValue(Grid.ColumnProperty, 1);
+            arrayOfBlack[5].SetValue(Grid.ColumnProperty, 3);
+            arrayOfBlack[6].SetValue(Grid.ColumnProperty, 5);
+            arrayOfBlack[7].SetValue(Grid.ColumnProperty, 7);
+
+            arrayOfBlack[8].SetValue(Grid.RowProperty, 5);
+            arrayOfBlack[9].SetValue(Grid.RowProperty, 5);
+            arrayOfBlack[10].SetValue(Grid.RowProperty, 5);
+            arrayOfBlack[11].SetValue(Grid.RowProperty, 5);
+
+            arrayOfBlack[8].SetValue(Grid.ColumnProperty, 0);
+            arrayOfBlack[9].SetValue(Grid.ColumnProperty, 2);
+            arrayOfBlack[10].SetValue(Grid.ColumnProperty, 4);
+            arrayOfBlack[11].SetValue(Grid.ColumnProperty, 6);
+        }
+
+        private void gameField_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var height = gameField.ActualHeight;
+            var width = gameField.ActualWidth;
+            var heightOfOneSell = (int)(height / 8);
+            var WidthOfOneSell = (int)(width / 8);
+            var mousePoint = Mouse.GetPosition(gameField);
+            var colomnNumber = (int)(mousePoint.X / WidthOfOneSell);
+            var rowNumber = (int)(mousePoint.Y / heightOfOneSell);
+            if (ellipse == null)
+            {
+                for (int i = 0; i < arrayOfWhite.Count && ellipse == null; i++)
+                {
+                    if ((int)arrayOfWhite[i].GetValue(Grid.ColumnProperty) == colomnNumber && (int)arrayOfWhite[i].GetValue(Grid.RowProperty) == rowNumber)
+                    {
+                        ellipse = arrayOfWhite[i];
+                    }
+                }
+
+                for (int i = 0; i < arrayOfBlack.Count && ellipse == null; i++)
+                {
+                    if ((int)arrayOfBlack[i].GetValue(Grid.ColumnProperty) == colomnNumber && (int)arrayOfBlack[i].GetValue(Grid.RowProperty) == rowNumber)
+                    {
+                        ellipse = arrayOfBlack[i];
+                    }
+                }
+            }
+            else
+            {
+                ellipse.SetValue(Grid.ColumnProperty, colomnNumber);
+                ellipse.SetValue(Grid.RowProperty, rowNumber);
+                ellipse = null;
+            }
+        }
+    }
+}
---
+++ Lab5/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly PointCollection Points = new PointCollection();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Path_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var mousePoint = Mouse.GetPosition(paintField);
+            DrawFigures(mousePoint);
+        }
+
+        private void DrawFigures(Point coords)
+        {
+            Shape figure = null;
+            if (radioEllips.IsChecked == true)
+            {
+                figure = GetEllipse(coords);
+            }
+
+            if (radioLine.IsChecked == true)
+            {
+                figure = GetLine(coords);
+            }
+
+            if (radioPlygon.IsChecked == true)
+            {
+                figure = GetPolygon(coords);
+            }
+
+            if (radioRectangle.IsChecked == true)
+            {
+                figure = GetRectangle(coords);
+            }
+
+            if (radioSquare.IsChecked == true)
+            {
+                figure = GetSquare(coords);
+            }
+
+            if (figure != null)
+            {
+                paintField.Children.Add(figure);
+                Points.Clear();
+            }
+        }
+
+        private SolidColorBrush GetBrush()
+        {
+            if (red.IsChecked == true)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+
+            if (black.IsChecked == true)
+            {
+                return new SolidColorBrush(Colors.Black);
+            }
+
+            if (purple.IsChecked == true)
+            {
+                return new SolidColorBrush(Colors.Purple);
+            }
+
+            if (pink.IsChecked == true)
+            {
+                return new SolidColorBrush(Colors.Pink);
+            }
+
+            if (yellow.IsChecked == true)
+            {
+                return new SolidColorBrush(Colors.Yellow);
+            }
+
+            if (blue.IsChecked == true)
+            {
+                return new SolidColorBrush(Colors.Blue);
+            }
+
+            return new SolidColorBrush(Colors.White);
+        }
+
+        private Ellipse GetEllipse(Point point)
+        {
+            var ellipse = new Ellipse
+            {
+                Height = 90,
+                Width = 90,
+                Fill = GetBrush(),
+                Margin = new Thickness(point.X, point.Y, 0, 0)
+            };
+            return ellipse;
+        }
+
+        private Line GetLine(Point point)
+        {
+            if (Points.Count == 1)
+            {
+                var line = new Line
+                {
+                    //Margin = new Thickness(point.X, point.Y, 0, 0),
+                    X1 = Points[0].X,
+                    X2 = point.X,
+                    Y1 = Points[0].Y,
+                    Y2 = point.Y,
+                    Stroke = GetBrush(),
+                    StrokeThickness = 2,
+                    Visibility = Visibility.Visible
+                };
+
+                return line;
+            }
+
+            Points.Add(point);
+            return null;
+        }
+
+        private Polygon GetPolygon(Point point)
+        {
+            if (Points.Count == 5)
+            {
+                Points.Add(point);
+                var polygon = new Polygon
+                {
+                    Fill = GetBrush(),
+                    Visibility = Visibility.Visible,
+                    Points = Points,
+                };
+
+                return polygon;
+            }
+
+            Points.Add(point);
+            return null;
+        }
+
+        private Rectangle GetRectangle(Point point)
+        {
+            var rectangle = new Rectangle()
+            {
+                Height = 90,
+                Width = 180,
+                Fill = GetBrush(),
+                Margin = new Thickness(point.X, point.Y, 0, 0),
+            };
+
+            return rectangle;
+        }
+
+        private Rectangle GetSquare(Point point)
+        {
+            var rectangle = new Rectangle()
+            {
+                Height = 50,
+                Width = 50,
+                Fill = GetBrush(),
+                Margin = new Thickness(point.X, point.Y, 0, 0),
+            };
+
+            return rectangle;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            paintField.Children.Clear();
+        }
+    }
+}
